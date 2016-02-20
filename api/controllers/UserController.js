05930bb94dd088cea5006bf67968db5b29aaebb0
Syed Abidur Rahman <aabid048@gmail.com>
2016-02-21 03:01:48 +0600

    Sign up form has been shown.

---
+++ api/controllers/UserController.js
@@ -0,0 +1,12 @@
+/**
+ * User Controller
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    new: function (req, res) {
+        res.view();
+    }
+};
