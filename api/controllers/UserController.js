97c6ba6e2cceea95cc5f80589725924190c18aaf
Syed Abidur Rahman <aabid048@gmail.com>
2016-02-21 13:37:05 +0600

    Flash policy has been added to shown validation error.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,11 +7,18 @@
 
 module.exports = {
     new: function (req, res) {
-        res.view();
+        return res.view();
     },
-    create: function(req, res, next) {
+    create: function(req, res) {
         User.create(req.params.all(), function(err, user) {
-            if (err) return next(err);
+            if (err) {
+                req.session.flash = {
+                    err: err
+                };
+                return res.redirect('/user/new');
+            }
+
+            req.session.flash = {};
             return res.json(user);
         });
     }
