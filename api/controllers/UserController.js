0b76677b6ca1556f4609ad3eaa928e72ae34a0f8
Syed Abidur Rahman <aabid048@gmail.com>
2016-02-21 15:26:07 +0600

    User editing has been done.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,10 +6,11 @@
  */
 
 module.exports = {
-    index: function(req, res, next) {
-        User.find(function(err, users) {
-            if (err) { return next(err); }
-            if (!users) { return next('No user has been found.'); }
+    index: function (req, res, next) {
+        User.find(function (err, users) {
+            if (err) return next(err);
+            if (!users) return next('No user has been found.');
+
             return res.view({
                 users: users
             });
@@ -18,8 +19,8 @@ module.exports = {
     new: function (req, res) {
         return res.view();
     },
-    create: function(req, res) {
-        User.create(req.params.all(), function(err, user) {
+    create: function (req, res) {
+        User.create(req.params.all(), function (err, user) {
             if (err) {
                 req.session.flash = {
                     err: err
@@ -28,16 +29,35 @@ module.exports = {
             }
 
             req.session.flash = {};
-            return res.redirect('/user/show/'+user.id);
+            return res.redirect('/user/show/' + user.id);
         });
     },
-    show: function(req, res, next) {
-        User.findOne(req.param('id'), function(err, user) {
-            if (err) { return next(err); }
-            if (!user) { return next('User has not been found.'); }
+    show: function (req, res, next) {
+        User.findOne(req.param('id'), function (err, user) {
+            if (err) return next(err);
+            if (!user) return next('User has not been found.');
+
             return res.view({
                 user: user
             });
+        });
+    },
+    edit: function (req, res, next) {
+        User.findOne(req.param('id'), function (err, user) {
+            if (err) return next(err);
+            if (!user) return next('User has not been found.');
+
+            return res.view({
+                user: user
+            });
+        });
+    },
+    update: function (req, res) {
+        User.update(req.param('id'), req.params.all(), function (err) {
+            if (err) return res.redirect('/user/edit/' + req.param('id'));
+
+            req.session.flash = {};
+            return res.redirect('/user/show/' + req.param('id'));
         });
     }
 };
